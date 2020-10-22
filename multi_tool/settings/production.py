d67ff2bdf45ecf90ea3b16d0cda6d2785e30c5db
Leighton <LPennicott3@gmail.com>
2020-10-22 07:09:36 -0400

    Fixed Production Settings

--- multi_tool/settings/production.py
+++ multi_tool/settings/production.py
@@ -6,8 +6,7 @@ import dj_database_url
 import django_heroku
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEBUG = False
 
